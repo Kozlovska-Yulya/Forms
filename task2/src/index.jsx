d1ab621a4a400c894a3584308788071488652e21
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-09-27 15:45:00 +0300

    Controlled component - User form

---
+++ task2/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+import UseForm from './UseForm';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<UseForm />, rootElement);
