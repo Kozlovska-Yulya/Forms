d9d946600e4d22e9599e30cfc274754eeea8ff39
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-09-29 16:42:32 +0300

    list filtration

--- task4/src/UsersList.jsx
+++ task4/src/UsersList.jsx
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const UsersList = ({ users }) => {
-  return (
-    <div>
-      <p>Найдено пользователей: {users.length}</p>
-      <ul className="users">
-        {users.map((user, index) => (
-          <li key={index} className="user">
-            <span className="user__name">{user.name}</span>
-            <span className="user__age">{user.age}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+class UsersList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filterText: '', // Исходное значение фильтра
+    };
+  }
+
+  handleFilterChange = (e) => {
+    this.setState({ filterText: e.target.value });
+  };
+
+  render() {
+    const { users } = this.props;
+    const { filterText } = this.state;
+
+    // Фильтрация пользователей на основе введенного текста
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(filterText.toLowerCase())
+    );
+
+    return (
+      <div>
+        <div className="filter">
+          <input
+            type="text"
+            className="filter__input"
+            placeholder="Фильтр по имени"
+            value={filterText}
+            onChange={this.handleFilterChange}
+          />
+        </div>
+        <ul className="users">
+          {filteredUsers.map((user, index) => (
+            <li className="user" key={index}>
+              <span className="user__name">{user.name}</span>
+              <span className="user__age">{user.age}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default UsersList;
